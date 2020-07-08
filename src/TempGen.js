8e47a8e81da96f29ba47cdfb1653e2f28ebea50a
l.jackson96 <l.jackson96@outlook.com>
2020-07-08 11:29:12 -0700

    added download functionality

--- src/TempGen.js
+++ src/TempGen.js
@@ -1,174 +1,210 @@
-import React, { Component } from 'react';
-import "./Generator.css"
+import React, { Component } from "react";
+import "./Generator.css";
+import * as svg from "save-svg-as-png";
 
 const initialState = {
-    toptext: "",
-    bottomtext: "",
-    isTopDragging: false,
-    isBottomDragging: false,
-    topX: "50%",
-    topY: "10%",
-    bottomX: "50%",
-    bottomY: "90%"
-}
+  toptext: "",
+  bottomtext: "",
+  isTopDragging: false,
+  isBottomDragging: false,
+  topX: "50%",
+  topY: "10%",
+  bottomX: "50%",
+  bottomY: "90%",
+};
 
 class TempGen extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentImage: "",
+      // modalIsOpen: false,
+      // currentImagebase64: null,
+      ...initialState,
+    };
+  }
 
-    constructor() {
-        super();
-        this.state = {
-            currentImage: "",
-            // modalIsOpen: false,
-            // currentImagebase64: null,
-            ...initialState
-        };
+  getStateObj = (e, type) => {
+    let rect = this.imageRef.getBoundingClientRect();
+    const xOffset = e.clientX - rect.left;
+    const yOffset = e.clientY - rect.top;
+    let stateObj = {};
+    if (type === "bottom") {
+      stateObj = {
+        isBottomDragging: true,
+        isTopDragging: false,
+        bottomX: `${xOffset}px`,
+        bottomY: `${yOffset}px`,
+      };
+    } else if (type === "top") {
+      stateObj = {
+        isTopDragging: true,
+        isBottomDragging: false,
+        topX: `${xOffset}px`,
+        topY: `${yOffset}px`,
+      };
     }
+    return stateObj;
+  };
 
-    getStateObj = (e, type) => {
-        let rect = this.imageRef.getBoundingClientRect();
-        const xOffset = e.clientX - rect.left;
-        const yOffset = e.clientY - rect.top;
-        let stateObj = {};
-        if (type === "bottom") {
-            stateObj = {
-                isBottomDragging: true,
-                isTopDragging: false,
-                bottomX: `${xOffset}px`,
-                bottomY: `${yOffset}px`
-            }
-        } else if (type === "top") {
-            stateObj = {
-                isTopDragging: true,
-                isBottomDragging: false,
-                topX: `${xOffset}px`,
-                topY: `${yOffset}px`
-            }
-        }
-        return stateObj;
+  changeText = (event) => {
+    this.setState({
+      [event.currentTarget.name]: event.currentTarget.value,
+    });
+  };
+
+  handleMouseDown = (e, type) => {
+    const stateObj = this.getStateObj(e, type);
+    document.addEventListener("mousemove", (event) =>
+      this.handleMouseMove(event, type)
+    );
+    this.setState({
+      ...stateObj,
+    });
+  };
+
+  handleMouseMove = (e, type) => {
+    if (this.state.isTopDragging || this.state.isBottomDragging) {
+      let stateObj = {};
+      if (type === "bottom" && this.state.isBottomDragging) {
+        stateObj = this.getStateObj(e, type);
+      } else if (type === "top" && this.state.isTopDragging) {
+        stateObj = this.getStateObj(e, type);
+      }
+      this.setState({
+        ...stateObj,
+      });
     }
+  };
 
-    changeText = (event) => {
-        this.setState({
-            [event.currentTarget.name]: event.currentTarget.value
-        });
-    }
+  handleMouseUp = (event) => {
+    document.removeEventListener("mousemove", this.handleMouseMove);
+    this.setState({
+      isTopDragging: false,
+      isBottomDragging: false,
+    });
+  };
 
-    handleMouseDown = (e, type) => {
-        const stateObj = this.getStateObj(e, type);
-        document.addEventListener('mousemove', (event) => this.handleMouseMove(event, type));
-        this.setState({
-            ...stateObj
-        })
-    }
+  convertSvgToImage = () => {
+    const svg = this.svgRef;
+    let svgData = new XMLSerializer().serializeToString(svg);
+    const canvas = document.createElement("canvas");
+    canvas.setAttribute("id", "canvas");
+    const svgSize = svg.getBoundingClientRect();
+    canvas.width = svgSize.width;
+    canvas.height = svgSize.height;
+    const img = document.createElement("img");
+    img.setAttribute(
+      "src",
+      "data:image/svg+xml;base64," + btoa(unescape(encodeURIComponent(svgData)))
+    );
+    img.onload = function () {
+      canvas.getContext("2d").drawImage(img, 0, 0);
+      const canvasdata = canvas.toDataURL("image/png");
+      console.log(svgData);
+      const a = document.createElement("a");
+      a.download = "meme.png";
+      a.href = canvasdata;
+      document.body.appendChild(a);
+      a.click();
+    };
+  };
 
-    handleMouseMove = (e, type) => {
-        if (this.state.isTopDragging || this.state.isBottomDragging) {
-            let stateObj = {};
-            if (type === "bottom" && this.state.isBottomDragging) {
-                stateObj = this.getStateObj(e, type);
-            } else if (type === "top" && this.state.isTopDragging) {
-                stateObj = this.getStateObj(e, type);
-            }
-            this.setState({
-                ...stateObj
-            });
-        }
+  svgToPng = () => {
+    // **** saveSvgAsPng ****
+    svg.saveSvgAsPng(document.getElementById("svg_ref"), "meme.png");
+  };
+
+  render() {
+    const image = this.props.meme;
+    var wrh = image.width / image.height;
+    var newWidth = 500;
+    var newHeight = newWidth / wrh;
+
+    const textStyle = {
+      fontFamily: "Impact",
+      fontSize: "50px",
+      textTransform: "uppercase",
+      fill: "#FFF",
+      stroke: "#000",
+      userSelect: "none",
     };
 
-    handleMouseUp = (event) => {
-        document.removeEventListener('mousemove', this.handleMouseMove);
-        this.setState({
-            isTopDragging: false,
-            isBottomDragging: false
-        });
-    }
+    return (
+      <div className="main-content">
+        <div className="meme-gen-modal">
+          <svg
+            id="svg_ref"
+            ref={(el) => {
+              this.svgRef = el;
+            }}
+            height={newHeight}
+            width={newWidth}
+          >
+            <image
+              ref={(el) => {
+                this.imageRef = el;
+              }}
+              xlinkHref={this.props.meme.url}
+              height={newHeight}
+              width={newWidth}
+            />
 
-    convertSvgToImage = () => {
-        const svg = this.svgRef;
-        let svgData = new XMLSerializer().serializeToString(svg);
-        const canvas = document.createElement("canvas");
-        canvas.setAttribute("id", "canvas");
-        const svgSize = svg.getBoundingClientRect();
-        canvas.width = svgSize.width;
-        canvas.height = svgSize.height;
-        const img = document.createElement("img");
-        img.setAttribute("src", "data:image/svg+xml;base64," + btoa(unescape(encodeURIComponent(svgData))));
-        img.onload = function () {
-            canvas.getContext("2d").drawImage(img, 0, 0);
-            const canvasdata = canvas.toDataURL("image/png");
-            console.log(svgData)
-            const a = document.createElement("a");
-            a.download = "meme.png";
-            a.href = canvasdata;
-            document.body.appendChild(a);
-            a.click();
-        };
-    }
+            <text
+              style={{ ...textStyle, zIndex: this.state.isTopDragging ? 4 : 1 }}
+              x={this.state.topX}
+              y={this.state.topY}
+              dominantBaseline="middle"
+              textAnchor="middle"
+              onMouseDown={(event) => this.handleMouseDown(event, "top")}
+              onMouseUp={(event) => this.handleMouseUp(event, "top")}
+            >
+              {this.state.toptext}
+            </text>
 
-    render() {
-        const image = this.props.meme;
-        var wrh = image.width / image.height;
-        var newWidth = 500;
-        var newHeight = newWidth / wrh;
-
-        const textStyle = {
-            fontFamily: "Impact",
-            fontSize: "50px",
-            textTransform: "uppercase",
-            fill: "#FFF",
-            stroke: "#000",
-            userSelect: "none"
-        }
-
-        return (
-            <div className="main-content">
-                <div className="meme-gen-modal" >
-                    <svg id="svg_ref"
-                        ref={el => { this.svgRef = el }}
-                        height={newHeight}
-                        width={newWidth}>
-
-                        <image
-                            ref={el => { this.imageRef = el }}
-                            xlinkHref={this.props.meme.url}
-                            height={newHeight}
-                            width={newWidth}
-                        />
-
-                        <text
-                            style={{ ...textStyle, zIndex: this.state.isTopDragging ? 4 : 1 }}
-                            x={this.state.topX}
-                            y={this.state.topY}
-                            dominantBaseline="middle"
-                            textAnchor="middle"
-                            onMouseDown={event => this.handleMouseDown(event, 'top')}
-                            onMouseUp={event => this.handleMouseUp(event, 'top')}
-                        >
-                            {this.state.toptext}
-                        </text>
-
-                        <text
-                            style={textStyle}
-                            dominantBaseline="middle"
-                            textAnchor="middle"
-                            x={this.state.bottomX}
-                            y={this.state.bottomY}
-                            onMouseDown={event => this.handleMouseDown(event, 'bottom')}
-                            onMouseUp={event => this.handleMouseUp(event, 'bottom')}
-                        >
-                            {this.state.bottomtext}
-                        </text>
-                    </svg>
-                    <div className="meme-form">
-                        <input className="form-control" type="text" name="toptext" id="toptext" placeholder="Add text to the top" onChange={this.changeText} />
-                        <input className="form-control" type="text" name="bottomtext" id="bottomtext" placeholder="Add text to the bottom" onChange={this.changeText} />
-                    </div>
-                    <button onClick={() => this.props.toggleSelected()} className="btn btn-primary">Back to Gallery</button>
-                    <button onClick={() => this.convertSvgToImage()} className="btn btn-primary">Download Meme :)</button>
-                </div>
-            </div>
-        );
-    }
+            <text
+              style={textStyle}
+              dominantBaseline="middle"
+              textAnchor="middle"
+              x={this.state.bottomX}
+              y={this.state.bottomY}
+              onMouseDown={(event) => this.handleMouseDown(event, "bottom")}
+              onMouseUp={(event) => this.handleMouseUp(event, "bottom")}
+            >
+              {this.state.bottomtext}
+            </text>
+          </svg>
+          <div className="meme-form">
+            <input
+              className="form-control"
+              type="text"
+              name="toptext"
+              id="toptext"
+              placeholder="Add text to the top"
+              onChange={this.changeText}
+            />
+            <input
+              className="form-control"
+              type="text"
+              name="bottomtext"
+              id="bottomtext"
+              placeholder="Add text to the bottom"
+              onChange={this.changeText}
+            />
+          </div>
+          <button
+            onClick={() => this.props.toggleSelected()}
+            className="btn btn-primary"
+          >
+            Back to Gallery
+          </button>
+          <button onClick={this.svgToPng} className="btn btn-primary">
+            Download Meme :D
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default TempGen;
